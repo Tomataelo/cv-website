639d8aa2fcb3d08a83d0d876bb8e34a2718d4f09
tomatowic <matrix2222matrix@gmail.com>
2025-06-23 15:39:04 +0200

    Adding a Git icon

--- src/components/RightSide/Slider.tsx
+++ src/components/RightSide/Slider.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
-import { FaJava, FaPhp, FaJsSquare, FaDatabase, FaReact, FaDocker } from "react-icons/fa";
+import { FaJava, FaPhp, FaJsSquare, FaDatabase, FaReact, FaDocker, FaGitAlt } from "react-icons/fa";
 
 const icons = [
     { icon: <FaJava size={60} color="#f7b731" /> },
@@ -8,7 +8,8 @@ const icons = [
     { icon: <FaDocker size={60} color="#2496ed" /> },
     { icon: <FaDatabase size={60} color="#2c3e50" /> },
     { icon: <FaJsSquare size={60} color="#f0db4f" /> },
-    { icon: <FaReact size={60} color="#61dbfb" /> }
+    { icon: <FaReact size={60} color="#61dbfb" /> },
+    { icon: <FaGitAlt size={60} color="#F35530" /> }
 ];
 
 const Slider: React.FC = () => {
